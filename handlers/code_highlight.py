3df7a5a7dd3507a21351a772a2a0d1ee0fc91350
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-09 20:01:28 +0300

    💄 improve code highlight stype

--- handlers/code_highlight.py
+++ handlers/code_highlight.py
@@ -4,16 +4,16 @@ import urllib.parse
 from io import BytesIO
 from typing import Optional, Union
 
-from PIL import Image
-from picode import picode
+from PIL.Image import Image, open as open_image
+from pygments.formatters.img import ImageFormatter
+from pygments.lexers import get_lexer_by_name, guess_lexer
 from telethon.events import NewMessage
 from telethon.tl.custom import Message
 
+from misc.base16monokai import MonokaiDark
 
-BASE_URL = 'https://carbon.now.sh'
 
-
-def build_url(code: str, lang):
+def build_carbon_url(code: str, lang: Optional[str]):
     qs = {
         'bg': 'rgba(37,37,37,1)',
         't': 'base16-dark',
@@ -22,48 +22,100 @@ def build_url(code: str, lang):
         'l': lang or 'auto',
     }
     qs = urllib.parse.urlencode(qs)
-    return BASE_URL + '/?' + qs
+    return 'https://carbon.now.sh/?' + qs
 
 
-def build_image(code: str, lang: Optional[str]) -> Image:
-    image = picode.to_pic(
-        code=code,
-        language=lang,
-        font_name=picode.DEFAULT_FONT_NAME_WIN,
+def get_lexer(lang=None, code=None):
+    if lang:
+        return get_lexer_by_name(lang)
+    if code:
+        return guess_lexer(code, stripall=True)
+    raise ValueError("Can't setup lexer.")
+
+
+def get_code_image(code: str, lang: Optional[str] = None) -> Image:
+    lexer = get_lexer(code=code, lang=lang)
+    formatter = ImageFormatter(
+        image_format="PNG",
+        font_size=32,
+        image_pad=100,
+        line_numbers=True,
+        line_number_bg='#22231e',
+        line_number_fg='#ddd',
+        line_number_bold=False,
+        line_number_separator=True,
+        line_number_pad=10,
+        style=MonokaiDark,
     )
+    tokens = lexer.get_tokens(code)
+    file = BytesIO()
+    formatter.format(tokens, file)
+    file.seek(0)
+    image = open_image(file)
+    return image
+
+
+def prettify_image(image: Image):
     return image
 
 
 @contextlib.contextmanager
 def load_image(code: str, lang: Optional[str] = None):
-    image = build_image(code, lang)
+    for line in code.split('\n'):
+        if len(line) > 150:
+            raise ValueError('Line are to long.')
+    image = get_code_image(code, lang)
+    image = prettify_image(image)
     file = BytesIO()
     image.save(file, 'PNG')
     file.seek(0)
     file.name = 'foo.png'
-    link = build_url(code, lang)
+    link = build_carbon_url(code, lang)
     link = f'[carbon]({link})'
     yield file, link
     file.close()
     image.close()
 
 
+async def send_image(event, code: str, lang: Optional[str] = None):
+    try:
+        with load_image(code, lang) as (file, link):
+            await asyncio.gather(
+                event.respond(link, file=file),
+                event.delete(),
+            )
+    except ValueError:
+        await event.delete()
+
+
 async def highlight_own(event):
     code = event.pattern_match.group(1)
     code = code.strip()
-    with load_image(code) as (file, link):
-        await asyncio.gather(
-            event.respond(link, file=file),
-            event.delete(),
-        )
+    await send_image(event, code)
 
 
 async def highlight_reply(event: Union[Message, NewMessage.Event]):
     lang = event.pattern_match.group(1)
     reply_msg = await event.get_reply_message()  # type: Message
     code = reply_msg.raw_text
-    with load_image(code, lang) as (file, link):
-        await asyncio.gather(
-            reply_msg.reply(link, file=file),
-            event.delete(),
-        )
+    await send_image(event, code, lang)
+
+
+def main():
+    code = """
+def foo(bar: str):
+    a = 43
+    b = lambda x: x + 1
+    a = b(a)
+
+class Foo(object):
+    def hurma(heh):
+        heh += 432
+        print(heh)
+    """
+    image = get_code_image(code, 'python')
+    image.show()
+
+
+if __name__ == '__main__':
+    main()
