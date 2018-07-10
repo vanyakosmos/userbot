dd01b98bc1bd9e49c623217688c2bf1b111492ae
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-10 21:48:00 +0300

    📝 prettier code image

--- handlers/code_highlight.py
+++ handlers/code_highlight.py
@@ -4,7 +4,7 @@ import urllib.parse
 from io import BytesIO
 from typing import Optional, Union
 
-from PIL.Image import Image, open as open_image
+from PIL import Image, ImageDraw, ImageFilter
 from pygments.formatters.img import ImageFormatter
 from pygments.lexers import get_lexer_by_name, guess_lexer
 from telethon.events import NewMessage
@@ -33,12 +33,12 @@ def get_lexer(lang=None, code=None):
     raise ValueError("Can't setup lexer.")
 
 
-def get_code_image(code: str, lang: Optional[str] = None) -> Image:
+def generate_code_image(code: str, lang: Optional[str] = None):
     lexer = get_lexer(code=code, lang=lang)
     formatter = ImageFormatter(
         image_format="PNG",
         font_size=32,
-        image_pad=100,
+        image_pad=30,
         line_numbers=True,
         line_number_bg='#22231e',
         line_number_fg='#ddd',
@@ -51,11 +51,48 @@ def get_code_image(code: str, lang: Optional[str] = None) -> Image:
     file = BytesIO()
     formatter.format(tokens, file)
     file.seek(0)
-    image = open_image(file)
+    image = Image.open(file).convert('RGBA')
     return image
 
 
-def prettify_image(image: Image):
+def add_corners(im, rad):
+    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+    alpha = Image.new('L', im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    im.putalpha(alpha)
+    return im
+
+
+def add_shadow(im: Image.Image, pad=100):
+    # bg
+    w, h = im.size
+    back = Image.new('RGBA', (w + pad, h + pad), '#fff')
+    # shadow
+    off = pad // 2
+    back.paste((0, 0, 0, 100), (
+        off - 10,
+        off + 20,
+        w + off + 10,
+        h + off + 20,
+    ))
+    for i in range(10):
+        back = back.filter(ImageFilter.BLUR)
+    # image
+    off = pad // 2
+    back.paste(im, (off, off), mask=im)
+    return back
+
+
+def get_code_image(code: str, lang: Optional[str] = None) -> Image.Image:
+    image = generate_code_image(code, lang)
+    image = add_corners(image, 10)
+    image = add_shadow(image)
     return image
 
 
@@ -67,13 +104,12 @@ def load_image(code: str, lang: Optional[str] = None):
         if len(line) > 150:
             raise ValueError('Line are to long.')
     image = get_code_image(code, lang)
-    image = prettify_image(image)
     file = BytesIO()
     image.save(file, 'PNG')
     file.seek(0)
     file.name = 'foo.png'
     link = build_carbon_url(code, lang)
-    link = f'[carbon]({link})'
+    link = f'[carbon]({link})' if len(link) < 1000 else None
     yield file, link
     file.close()
     image.close()
