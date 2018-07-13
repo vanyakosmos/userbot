2fd63d5d48ba3eba32e79bcad3cecee78917b84a
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-13 22:25:35 +0300

    📝 better set representation

--- handlers/calculator.py
+++ handlers/calculator.py
@@ -7,13 +7,15 @@ from telethon.tl.custom import Message
 
 async def calculator(event: Union[Message, NewMessage.Event]):
     expression = event.pattern_match.group(1)
-    expression = re.sub(r'[^\d+\-*/().%&|{}\[\]]', '', expression)
+    expression = re.sub(r'[^\d+\-*/().,%&|{}\[\]]', '', expression)
 
     if not 0 < len(expression) < 100:
         await event.reply("Bad expression.")
         return
     try:
         answer = eval(expression)
+        if answer == set():
+            answer = '{}'
         await event.edit(f'```{expression}\n> {answer}```')
         # await event.reply(f'`{expression} = {answer}`')
     except Exception as e:
